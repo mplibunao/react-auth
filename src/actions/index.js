9cf22aefa1838d8007b4534af2a81a9f29cdaddb
mplibunao <markpaololibunao@gmail.com>
2017-10-31 17:25:14 +0800

    created action creator which removes jwt token from localStorage and returns action for authenticated state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
-import { AUTH_USER, AUTH_ERROR } from './types'; 
+import {
+  AUTH_USER,
+  AUTH_ERROR,
+  UNAUTH_USER
+} from './types'; 
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -33,4 +37,9 @@ export function authError(error) {
     type: AUTH_ERROR,
     payload: error
   };
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+  return { type: UNAUTH_USER };
 }
