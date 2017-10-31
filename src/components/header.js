7132be197763a055ed73cb848b9cf5f40ca4bdb9
mplibunao <markpaololibunao@gmail.com>
2017-10-31 16:57:42 +0800

    fixed jsx for li

--- src/components/header.js
+++ src/components/header.js
@@ -5,13 +5,12 @@ import { Link } from 'react-router-dom';
 class Header extends Component {
     render(){
         const { authenticated } = this.props;
-        const showHeader = authenticated ? <div>Welcome!</div> : <Link to="/signin">Sign in</Link>;
+        const showHeader = authenticated ? <li className="nav-item">Welcome!</li> : <li className="nav-item"><Link to="/signin">Sign in</Link></li>;
         return (
             <nav className="navbar navbar-light">
+                <Link to="/" className="navbar-brand">Home</Link>
                 <ul className="nav navbar-nav">
-                    <li className="nav-item">
-                        {showHeader}
-                    </li>
+                    {showHeader}
                 </ul>
             </nav>
         );
