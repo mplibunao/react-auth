c9c1bd1a598525226f1adcbde6ea0a0dae46e33b
mplibunao <markpaololibunao@gmail.com>
2017-11-02 22:34:55 +0800

    Added redux form validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -2,16 +2,92 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import * as actions from "../../actions";
 import Header from "../header";
+import "../../styles/index.css";
 
 class Signup extends Component {
+
+    handleFormSubmit(values) {
+        
+    }
+    
+    renderField(field) {
+        // field.meta.error contains the error message from the returned error object
+        const { error, touched } = field.meta;
+        return (
+            <div>
+                <fieldset className="form-group">
+                    <label>{field.label}</label>
+                    <input type={field.type} className="form-control" {...field.input} />
+                </fieldset>
+                <div className="error">
+                    {/* if field was touched show error else, show empty string */}
+                    { touched ? error : "" }
+                </div>
+            </div>
+        );
+    }
+
     render() {
         return (
             <div>
                 <Header />
-                <div>Sign up</div>
+                <form>
+                    <Field label="Email:"
+                        name="email"
+                        type="text"
+                        component={this.renderField}
+                    />
+                    <Field label="Password:"
+                        name="password"
+                        type="password"
+                        component={this.renderField}
+                    />
+                    <Field label="Confirm Password:"
+                        name="passwordConfirm"
+                        type="password"
+                        component={this.renderField}
+                    />
+                    <button action="submit" className="btn btn-primary">Sign up!</button>
+                </form>
             </div>
         );
     }
 }
 
-export default Signup;
+/**
+ * When you submit/touch a field, redux form validates the field using the validate function
+ * Returns an error object; Property names of the object correspond with the `name` of the field
+ * Similar to error, values is also an object where its properties corresponds to the `name` of the fields but this time for the value
+ * @param {Object} values 
+ */
+function validate(values) {
+    // Instantiate empty errors object
+    const errors = {};
+    // Get (destructure) the values of the input fields through the values object
+    const { email, password, passwordConfirm } = values;
+
+    if (password !== passwordConfirm) {
+        errors.password = "Passwords must match";
+    }
+    
+    if (!password) {
+        errors.password = "Please enter a password";
+    } else if (password.length < 4) {
+        errors.password = "Password must be longer than 3 characters";
+    } else if ( password.search(/\d/g) === false || password.search(/[A-z]/g) === false ) {
+        errors.password = "Password must be alphanumeric";
+    } else if ( password.search(/\s/g) === true ) {
+        errors.password = "Password must not contain any whitespace";
+    }
+
+
+    
+    // Return errors object; If empty, then show no validation error, else show the validation error
+    // for a particular field corresponding to the field `name`
+    return errors;
+} 
+
+export default reduxForm({
+    validate,
+    form: "signup",
+})(Signup);
