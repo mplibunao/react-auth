d6f12cb811bcb492d86076510b3f037b97e535ce
mplibunao <markpaololibunao@gmail.com>
2017-11-12 05:29:51 +0800

    automatic authentication

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
 
-import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from "./components/auth/signup";
@@ -13,11 +12,19 @@ import Feature from "./components/feature";
 import Welcome from "./components/welcome";
 import RequireAuth from "./components/auth/require_auth";
 import reducers from './reducers';
+import { AUTH_USER } from "./actions/types";
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem("token");
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
+
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <BrowserRouter>
       <div>
         <Switch>
