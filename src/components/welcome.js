195dc21a22db8426febe57ab0734f62040f1fc09
mplibunao <markpaololibunao@gmail.com>
2017-11-12 05:24:00 +0800

    default landing page

---
+++ src/components/welcome.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./header";
+
+export default () => (
+    <div>
+        <Header />
+        <div>Welcome to our slice of paradise</div>
+    </div>
+)
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from "./components/auth/signup";
 import Feature from "./components/feature";
+import Welcome from "./components/welcome";
 import RequireAuth from "./components/auth/require_auth";
 import reducers from './reducers';
 
@@ -24,7 +25,7 @@ ReactDOM.render(
           <Route path="/signin" component={Signin} />
           <Route path="/signout" component={Signout} />
           <Route path="/signup" component={Signup} />
-          <Route path="/" component={App} />
+          <Route path="/" component={Welcome} />
         </Switch>
       </div>
     </BrowserRouter>
